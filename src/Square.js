b2a3f5162aa8fde27e99d3299023c58a711c612d
Vadim Pashaev <vmp@live.ru>
2018-10-08 02:20:08 +0400

    s

---
+++ src/Square.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react'
+import './Square.css'
+
+class Square extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            value: null
+        }
+    }
+
+    render() {
+        return (
+            <button className="square" onClick={() => this.props.onClick()}>
+                {this.props.value}
+            </button>
+        )
+    }
+}
+
+export default Square
