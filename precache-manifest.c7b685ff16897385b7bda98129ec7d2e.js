724149a2394e46ec630234b7bcaf799b9be903e8
Vadim Pashaev <vmp@live.ru>
2018-10-08 19:28:47 +0400

    Updates

---
+++ precache-manifest.c7b685ff16897385b7bda98129ec7d2e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "20e63a8b74dcd8a49dbd",
+    "url": "/reactjsorg-tut/static/js/runtime~main.20e63a8b.js"
+  },
+  {
+    "revision": "c9d6ccf1c946b09e3304",
+    "url": "/reactjsorg-tut/static/js/main.c9d6ccf1.chunk.js"
+  },
+  {
+    "revision": "36afbc2ff7a701f5e8ee",
+    "url": "/reactjsorg-tut/static/js/1.36afbc2f.chunk.js"
+  },
+  {
+    "revision": "c9d6ccf1c946b09e3304",
+    "url": "/reactjsorg-tut/static/css/main.dff2079f.chunk.css"
+  },
+  {
+    "revision": "49d8cb01f57e5c998dd582539fe298e3",
+    "url": "/reactjsorg-tut/index.html"
+  }
+];
